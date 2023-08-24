68f0bc7ffaefee544734523bde8146440af05a20
Toby <tobyyzhang@gmail.com>
2023-08-24 10:03:54 -0400

    update

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
         <meta name="twitter:title" content="Deepfake | Create Deepfakes"/>
         <meta name="twitter:description" content="Generate deepfake videos with AI."/>
         <script src="https://assets.lemonsqueezy.com/lemon.js" defer></script>
+        <link href="https://api.fontshare.com/v2/css?f[]=clash-display@600&display=swap" rel="stylesheet"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,200;10..48,300;10..48,400;10..48,500;10..48,600;10..48,700;10..48,800&display=swap" rel="stylesheet"></link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily :{
-        'bricolage': ['Bricolage Grotesque', 'sans-serif'],
+        'clash': ['Clash Display', 'sans-serif'],
       },
     },
   },
