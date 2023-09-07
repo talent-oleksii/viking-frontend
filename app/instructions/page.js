c96376f3553ddd7a05d98a9f68f26a3ac46d2abf
Toby <tobyyzhang@gmail.com>
2023-09-06 23:54:16 -0400

    update

--- app/instructions/page.js
+++ app/instructions/page.js
@@ -74,24 +74,6 @@ export default function Home() {
             },
           }}
         >
-          {/* <motion.a
-            variants={FADE_DOWN_ANIMATION_VARIANTS}
-            href="https://twitter.com/intent/tweet?text=Follow%20these%20steps%20to%20earn%20free%20credits%20%F0%9F%91%87%0A%0A1.%20Link%20deepfake.pics%0A%0A2.%20Upload%20your%20video%0A%0A%28send%20me%20a%20DM%20after%20you%27re%20done%20%40tobytabi%29"
-            target="_blank"
-            rel="noreferrer"
-            className="mx-auto mb-5 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-5 sm:px-5 py-2 transition-colors hover:bg-blue-200"
-            onClick={() => {
-              va.track("Share on Twitter Button");
-            }}
-          >
-            <p className="text-sm font-semibold text-[#1d9bf0]">Instructions</p>
-          </motion.a> */}
-          {/* <motion.h1
-            className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-clash text-5xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem] pb-[2px]"
-            variants={FADE_DOWN_ANIMATION_VARIANTS}
-          >
-            <div>View your photos</div>
-          </motion.h1> */}
           <motion.p
             className="border border-[#1d9bf0] w-full sm:max-w-xl mt-10 text-center rounded-2xl px-4 py-12 sm:py-20 text-[#1d9bf0] bg-blue-100 flex flex-col gap-8 sm:gap-8"
             variants={FADE_DOWN_ANIMATION_VARIANTS}
@@ -113,13 +95,6 @@ export default function Home() {
               </span>
             </div>
           </motion.p>
-          {/* <motion.div
-            // className="group relative mx-auto mt-10 h-[350px] w-full overflow-hidden rounded-2xl border border-gray-200 sm:h-[600px] sm:w-[600px]"
-            className="group relative mx-auto mt-10 w-full overflow-hidden rounded-2xl border border-gray-200 focus:ring-0"
-            variants={FADE_DOWN_ANIMATION_VARIANTS}
-          >
-            <div className="">hello</div>
-          </motion.div> */}
         </motion.div>
         <motion.div></motion.div>
       </main>
