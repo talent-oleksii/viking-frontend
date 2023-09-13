7c54a9a69f1d0e062f6c22dc4829d608600eac47
Toby <tobyyzhang@gmail.com>
2023-09-13 05:11:38 -0400

    update

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -23,62 +23,6 @@ function PredictionPage({ params }) {
   const router = useRouter();
   console.log(params.id);
 
-  // const [image1, setImage1] = useState("");
-  // const [image2, setImage2] = useState("");
-  // const [image3, setImage3] = useState("");
-  // const [image4, setImage4] = useState("");
-  // const [image5, setImage5] = useState("");
-  // const [image6, setImage6] = useState("");
-  // const [image7, setImage7] = useState("");
-  // const [image8, setImage8] = useState("");
-  // const [image9, setImage9] = useState("");
-  // const [image10, setImage10] = useState("");
-  // const [image11, setImage11] = useState("");
-  // const [image12, setImage12] = useState("");
-  // const [image13, setImage13] = useState("");
-  // const [image14, setImage14] = useState("");
-  // const [image15, setImage15] = useState("");
-  // const [image16, setImage16] = useState("");
-  // const [image17, setImage17] = useState("");
-  // const [image18, setImage18] = useState("");
-  // const [image19, setImage19] = useState("");
-  // const [image20, setImage20] = useState("");
-
-  // const fetchImages = async () => {
-  //   const { data, error } = await supabase
-  //     .from("users")
-  //     .select("*")
-  //     .eq("partial", params.id);
-
-  //   console.log(data);
-  //   console.log(data[0].result1);
-
-  //   setImage1(data[0].result1);
-  //   setImage2(data[0].result2);
-  //   setImage3(data[0].result3);
-  //   setImage4(data[0].result4);
-  //   setImage5(data[0].result5);
-  //   setImage6(data[0].result6);
-  //   setImage7(data[0].result7);
-  //   setImage8(data[0].result8);
-  //   setImage9(data[0].result9);
-  //   setImage10(data[0].result10);
-  //   setImage11(data[0].result11);
-  //   setImage12(data[0].result12);
-  //   setImage13(data[0].result13);
-  //   setImage14(data[0].result14);
-  //   setImage15(data[0].result15);
-  //   setImage16(data[0].result16);
-  //   setImage17(data[0].result17);
-  //   setImage18(data[0].result18);
-  //   setImage19(data[0].result19);
-  //   setImage20(data[0].result20);
-  // };
-
-  // useEffect(() => {
-  //   fetchImages();
-  // }, []);
-
   // const [imageExists, setImageExists] = useState(false);
 
   // useEffect(() => {
@@ -217,33 +161,10 @@ function PredictionPage({ params }) {
             className="mt-10 sm:mt-16 grid sm:grid-cols-2 gap-6 sm:gap-6"
             variants={FADE_DOWN_ANIMATION_VARIANTS}
           >
-            {/* <img src={image1} alt="" className="rounded-2xl" />
-            <img src={image2} alt="" className="rounded-2xl" />
-            <img src={image3} alt="" className="rounded-2xl" />
-            <img src={image4} alt="" className="rounded-2xl" />
-            <img src={image5} alt="" className="rounded-2xl" />
-            <img src={image6} alt="" className="rounded-2xl" />
-            <img src={image7} alt="" className="rounded-2xl" />
-            <img src={image8} alt="" className="rounded-2xl" />
-            <img src={image9} alt="" className="rounded-2xl" />
-            <img src={image10} alt="" className="rounded-2xl" />
-            <img src={image11} alt="" className="rounded-2xl" />
-            <img src={image12} alt="" className="rounded-2xl" />
-            <img src={image13} alt="" className="rounded-2xl" />
-            <img src={image14} alt="" className="rounded-2xl" />
-            <img src={image15} alt="" className="rounded-2xl" />
-            <img src={image16} alt="" className="rounded-2xl" />
-            <img src={image17} alt="" className="rounded-2xl" />
-            <img src={image18} alt="" className="rounded-2xl" />
-            <img src={image19} alt="" className="rounded-2xl" />
-            <img src={image20} alt="" className="rounded-2xl" /> */}
             {/* {imageExists ? (
-              <div></div>
+              <div className="hidden"></div>
             ) : (
-              <div className="text-white text-2xl">
-                Your photos are still being generated. Please check back in 1
-                hour.
-              </div>
+              <div className=""></div>
             )} */}
             {existingImages.length > 0 ? (
               existingImages.map((url, index) => (
@@ -252,106 +173,6 @@ function PredictionPage({ params }) {
             ) : (
               <p></p>
             )}
-            {/* <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}1.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}2.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}3.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}4.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}5.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}6.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}7.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}8.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}9.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}10.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}11.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}12.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}13.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}14.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}15.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}16.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}17.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}18.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}19.png`}
-              alt=""
-              className="rounded-2xl"
-            />
-            <img
-              src={`https://remwbrfkzindyqlksvyv.supabase.co/storage/v1/object/public/results/${params.id}20.png`}
-              alt=""
-              className="rounded-2xl"
-            /> */}
           </motion.div>
         </motion.div>
         <motion.div></motion.div>
