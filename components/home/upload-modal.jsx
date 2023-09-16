3d928926f7a9fcfb509dc7394d5b42866baebb0f
Toby <tobyyzhang@gmail.com>
2023-09-16 00:32:52 -0400

    update text

--- components/home/upload-modal.jsx
+++ components/home/upload-modal.jsx
@@ -190,7 +190,7 @@ const UploadModal = ({ showUploadModal, setShowUploadModal }) => {
             </a>
             <h3 className="font-clash text-2xl font-bold">Upload Photos</h3>
             <Balancer className="text-sm text-gray-500 leading-6 sm:pb-0 pb-0.5">
-              Choose 10 to 20 photos of yourself.
+              10 to 20 selfies with plain background.
             </Balancer>
           </div>
           <form className="grid gap-5 bg-gray-50 px-4 sm:pt-8 sm:pb-8 pt-7 pb-5 md:px-16">
