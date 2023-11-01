aecf2291e866fdb8144ee27f2a43c4f2c1f7f849
talent-oleksii <oleksiikaravanov@gmail.com>
2023-11-01 13:04:35 +0200

    add env

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -1,7 +1,11 @@
 import Stripe from "stripe";
 
+
+const uuid = require('uuid');
+
 const stripe = new Stripe(
-    "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"
+    // "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"
+    process.env.STRIPE_SECRET_KEY
   );
 
 export async function POST(request) {
@@ -13,15 +17,19 @@ export async function POST(request) {
   // Determine priceID
   let priceID;
   if (orderOption == 8) {
-    priceID = "price_1NpP52J0xJPb1lZKeUH1vw9i";
+    // priceID = "price_1NpP52J0xJPb1lZKeUH1vw9i";
+    // priceID = 'price_1O7DD8J0xJPb1lZK9tf8LNc5';
+    priceID = process.env.PRICE_8;
   } else {
-    priceID = "price_1NpP64J0xJPb1lZKtqDfPTnZ";
+    // priceID = "price_1NpP64J0xJPb1lZKtqDfPTnZ";
+    // priceID = 'price_1O7DDgJ0xJPb1lZKO9CuKwLN';
+    priceID = process.env.PRICE_20;
   }
   console.log("Price ID: ", priceID);
 
   try {
     const session = await stripe.checkout.sessions.create({
-      success_url: `https://aiviking.com/${emailPrefix}`,
+      success_url: `https://aiviking.com/${uuid.v4()}`,
       line_items: [{ price: priceID, quantity: 1 }],
       mode: "payment",
       customer_email: email,
