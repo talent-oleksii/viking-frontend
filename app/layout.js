e6843fe848917351a4295b901cac5a6bd971d45f
talent-oleksii <oleksiikaravanov@gmail.com>
2023-11-28 21:14:44 +0200

    add

--- app/layout.js
+++ app/layout.js
@@ -59,7 +59,7 @@ export default function RootLayout({ children }) {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', 'YOUR_PIXEL_ID');
+          fbq('init', '726695176043348');
           fbq('track', 'PageView');
         `,
           }}
